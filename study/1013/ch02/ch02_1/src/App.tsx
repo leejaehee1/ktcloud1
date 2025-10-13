f5815d3aa66b958bf52effc1827be73c19585fc1
john <test@google.com>
2025-10-13 12:51:41 +0900

    1013 exam2

--- study/1013/ch02/ch02_1/src/App.tsx
+++ study/1013/ch02/ch02_1/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./components/Profile";
+import Demo from "./components/Demo";
 
 const children = [
   <li>
@@ -26,6 +27,9 @@ function App() {
     <ul>
       <li>
         <Profile />
+      </li>
+      <li>
+        <Demo />
       </li>
       {children}
     </ul>
--- study/1013/ch02/ch02_1/src/components/Demo.tsx
+++ study/1013/ch02/ch02_1/src/components/Demo.tsx
@@ -1,8 +1,14 @@
+import * as D from "../data";
+
 export default function Demo() {
   return (
     <>
       <h1>Demo</h1>
-      <p>This is a demo component</p>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} width={100} height={100} alt="avatar" />
+      <img src={D.randomImage()} width={100} height={100} alt="random image" />
     </>
   );
 }
