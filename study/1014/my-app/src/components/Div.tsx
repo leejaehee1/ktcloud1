af4ae4fa83144ff295a6b9e2f9f6c2397cda73a6
john <test@google.com>
2025-10-14 12:19:20 +0900

    1014 exam

---
+++ study/1014/my-app/src/components/Div.tsx
@@ -0,0 +1,7 @@
+export default function Div() {
+  return (
+    <div className="w-10 h-10 bg-red-500 border-4 border-blue-500 pb-5 mx-8 text-blue-500">
+      Div
+    </div>
+  );
+}
---
+++ study/1014/my-app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Div from "../components/Div";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-evenly w-full h-full">
+      <Div />
+      <Div />
+      <Div />
+      <Div />
+      <Div />
+    </div>
+  );
+}
