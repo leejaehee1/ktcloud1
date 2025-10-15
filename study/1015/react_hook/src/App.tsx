80021182134dcc83b00e815db6e357de7de7023a
john <test@google.com>
2025-10-15 12:46:13 +0900

    1015

---
+++ study/1015/react_hook/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "@fontsource/material-icons";
+import ReactHookPage from "./pages/ReactHookPage";
+
+function App() {
+  return <ReactHookPage />;
+}
+
+export default App;
