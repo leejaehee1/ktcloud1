f6dd10a52f0bbdc9cb776ec1b4e07eabde47463e
john <test@google.com>
2025-10-16 14:35:51 +0900

    10161434

--- study/1016/react-router/src/pages/Home.tsx
+++ study/1016/react-router/src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
+import Counter from "../components/Counter";
 
 type HomeProps = {
   title?: string;
 };
 
 const Home: FC<HomeProps> = ({ title }) => {
-  return <p>{title ?? "Home"}</p>;
+  return (
+    <div>
+      <p>{title ?? "Home"}</p>
+      <Counter />
+    </div>
+  );
 };
 
 export default Home;
