a63cd3456569e3ea3eb6cd646d209389f52494fa
john <test@google.com>
2025-10-22 10:27:51 +0900

    10221027

---
+++ study/1022/board/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "테스트 게시판" });
+});
+
+app.listen(3000);
