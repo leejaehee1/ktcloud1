5c3e52aa38823bd8a6cccb118aa5d878e4a9c4b7
john <test@google.com>
2025-11-05 09:39:13 +0900

    11050939

---
+++ study/1105/hook_exam/src/pages/ExamplePage.tsx
@@ -0,0 +1,10 @@
+import InputFocusExample from "../components/InputFocusExample";
+
+export default function ExamplePage() {
+  return (
+    <div>
+      <h1>ExamplePage</h1>
+      <InputFocusExample />
+    </div>
+  );
+}
