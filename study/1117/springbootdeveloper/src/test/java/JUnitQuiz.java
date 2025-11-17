0b354d85f21807014b5fe2146b572801a0676532
john <test@google.com>
2025-11-17 10:34:33 +0900

    11171034

--- study/1117/springbootdeveloper/src/test/java/JUnitQuiz.java
+++ study/1117/springbootdeveloper/src/test/java/JUnitQuiz.java
@@ -12,18 +12,33 @@ public class JUnitQuiz {
 
         // 1. 모든 변수가 null이 아닌지 확인
         assertThat(name1).isNotNull();
+        assertThat(name2).isNotNull();
+        assertThat(name3).isNotNull();
 
         // 2. name1과 name2가 같은지 확인
+        assertThat(name1).isEqualTo(name2);
+
         // 3. name1과 name3이 다른지 확인
+        assertThat(name1).isNotEqualTo(name3);
 
         int number1 = 15;
         int number2 = 0;
         int number3 = -5;
 
         // 4. number1은 양수인지 확인
+        assertThat(number1).isGreaterThan(0);
+        assertThat(number1).isPositive();
+
         // 5. number2은 0인지 확인
+        assertThat(number2).isZero();
+
         // 6. number3은 음수인지 확인
+        assertThat(number3).isNegative();
+
         // 7. number1은 number2보다 큰지 확인
+        assertThat(number1).isGreaterThan(number2);
+
         // 8. number3은 number2보다 작은지 확인
+        assertThat(number3).isLessThan(number2);
     }
 }
