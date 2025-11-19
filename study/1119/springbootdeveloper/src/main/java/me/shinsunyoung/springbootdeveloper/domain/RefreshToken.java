ea2d439815e4c58c7afeca68e50e1aa8680831c0
john <test@google.com>
2025-11-19 09:50:37 +0900

    11190950

---
+++ study/1119/springbootdeveloper/src/main/java/me/shinsunyoung/springbootdeveloper/domain/RefreshToken.java
@@ -0,0 +1,14 @@
+package me.shinsunyoung.springbootdeveloper.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RefreshToken {
+
+}
